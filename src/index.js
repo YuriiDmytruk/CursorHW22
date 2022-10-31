0e630e605c3f794434c881199b13dc2e7d0694c2
Yura Yura <yuriydmytrukr@gmail.com>
2022-10-31 11:24:05 +0200

    Start lecture 22

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import SignUp from './components/SignUp/SignUp.js'
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <SignUp />
+);
+
+
