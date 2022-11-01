13de44127155faeb83f2b380f9144e31f354d154
Yura Yura <yuriydmytrukr@gmail.com>
2022-11-01 08:20:02 +0200

    s

--- src/index.js
+++ src/index.js
@@ -8,17 +8,14 @@ import SignIn from "./components/SignIn/SignIn.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <div>
-    <SignIn/>
     <BrowserRouter>
       <div>
         <Routes>
           <Route path="/home" element={<Home />} />
           <Route path="/registration" element={<SignUp />} />
           <Route path="/signin" element={<SignIn />} />
-          <Route path="/" element={<Navigate to="/signin" replace={true} />} />
+          <Route path="*" element={<Navigate to="/signin" replace={true} />} />
         </Routes>
       </div>
     </BrowserRouter>
-  </div>
 );
