19cd22dfdaa20159ad37e6a77bd88082fc8622c7
Yura Yura <yuriydmytrukr@gmail.com>
2022-11-01 08:58:06 +0200

    s

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Route, BrowserRouter, Routes, NavLink } from "react-router-dom";
+import { Route, HashRouter, Routes } from "react-router-dom";
 
 import SignUp from "./components/SignUp/SignUp.jsx";
 import Home from "./components/Home/Home.jsx";
@@ -8,12 +8,12 @@ import SignIn from "./components/SignIn/SignIn.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
       <Routes>
         <Route path="/home" element={<Home />} />
         <Route path="/registration" element={<SignUp />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/CursorHW22/" element={<SignIn />} />
       </Routes>
-  </BrowserRouter>
+  </HashRouter>
 );
